1505176eae20ef78a50e7fa300adc060edf21194
johnson845173 <rebeirojohnson@gmail.com>
2022-10-31 23:08:58 +0530

    added get data

--- getdata.py
+++ getdata.py
@@ -1,6 +1,7 @@
 import requests
 
 def getdata(symbol):
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey=8SP5CFNLUHXR9F34&outputsize=compact'
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}\
+        &outputsize=full&apikey=8SP5CFNLUHXR9F34&outputsize=compact'
     data = requests.get(url).json()
     return data['Time Series (Daily)']
