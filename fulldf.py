f155b33ed8aa0eadf324c47d6aeff4bcc89229ba
johnson845173 <rebeirojohnson@gmail.com>
2022-11-01 17:00:50 +0530

    added data csv

--- fulldf.py
+++ fulldf.py
@@ -9,17 +9,17 @@ date_list =  []
 final_list =  []
 
 def full_df(symbols):
-    
     for symbol in symbols:
-        print(symbol)
+        print(symbol,end="|")
         required_data = getdata(symbol)
         for each_item in required_data:
-            if len(date_list) < 30:
+            if len(date_list) < 90:
                 date_list.append(each_item)
             else:
                 break
         for each_date in date_list:
             open,close = required_data[each_date]['1. open'],required_data[each_date]['4. close']
             Outdf.loc[len(Outdf.index)] = symbol,each_date,open,close,required_data[each_date]['3. low'],required_data[each_date]['2. high']  # type: ignore
-        
+    
+    print("")
     return Outdf
--- main.py
+++ main.py
@@ -1,39 +1,53 @@
+from fileinput import close
 import time
 from fulldf import full_df
 import pandas as pd
 from methods import find_trend,get_five_day_trend
 from stockname import sub_stock
 
-Outdf = pd.DataFrame(columns=["Stock","Day1","Day2","Day3","Day4","Day5","Day6","Day7","weekly","15 day","Monthly"])
-symbols = sub_stock[0:1]
+Outdf = pd.DataFrame(columns=["Stock","Day1","Day2","Day3","Day4","Day5","Day6","Day7","weekly","15 day","Monthly","Ltp","D1","D2","D3","D4","D5","D6","D7","D10","D15","D20","D25","D30","D45","D60","D90"])
+symbols = sub_stock
 
 for each_sub_stock in symbols:
-    main_df = full_df(each_sub_stock)
-    i = j = k = 0 
+    try:
+        main_df = full_df(each_sub_stock)
+        i = j = k = 0 
 
-    for symbol in each_sub_stock:
-        i = j + 30
-        stock_df = main_df.iloc[j:i,:]
-        j = i
+        for symbol in each_sub_stock:
+            i = j + 90
+            stock_df = main_df.iloc[j:i,:]
+            j = i
+            
+            close_price = stock_df['Close'].to_list()
+
+            cp = close_price
+
+            wk_trrend = get_five_day_trend(close_price)
+
+            present_price = float(close_price[0])
+
+            last_week_price = float(close_price[7])
+
+            last_15_price = float(close_price[15])
+
+            last_month_price = float(close_price[29])
+
+            week_diff,month_diff,fort_diff = find_trend(present_price,last_week_price,last_15_price,last_month_price)
+
+            Outdf.loc[len(Outdf.index)] = [symbol,wk_trrend[0],wk_trrend[1],wk_trrend[2],wk_trrend[3],wk_trrend[4],wk_trrend[5],wk_trrend[6],week_diff,fort_diff,month_diff,close_price[0],cp[1],cp[2],cp[3],cp[4],cp[5],cp[6],cp[7],cp[9],cp[14],cp[19],cp[24],cp[29],cp[44],cp[59],cp[89]] # type: ignore
+            
+            print(f"{symbol} Done", end="|")
+
+            Outdf.to_csv("data.csv")
+
+            time.sleep(12)
         
-        close_price = stock_df['Close'].to_list()
-        
-        five_data_trend = get_five_day_trend(close_price)
-
-        present_price = float(close_price[0])
-
-        last_week_price = float(close_price[7])
-
-        last_15_price = float(close_price[15])
-
-        last_month_price = float(close_price[29])
-
-        week_diff,month_diff,fort_diff = find_trend(present_price,last_week_price,last_15_price,last_month_price)
-
-        Outdf.loc[len(Outdf.index)] = [symbol,five_data_trend[0],five_data_trend[1],five_data_trend[2],five_data_trend[3],five_data_trend[4],five_data_trend[5],five_data_trend[6],week_diff,fort_diff,month_diff]  # type: ignore
-        
-        print(f"{symbol} done")
-        time.sleep(10)
+        print("")
+    except:
+        print("skipping")
+        time.sleep(12)
     
 
 print(Outdf.to_string())
+
+
