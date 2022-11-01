36042ce41d08a13f1d829c08fb5758cd363bb081
johnson845173 <rebeirojohnson@gmail.com>
2022-11-01 11:07:51 +0530

    added dataframe

--- getdata.py
+++ getdata.py
@@ -1,10 +1,9 @@
 import requests
 from automatic import rel
 
-data = rel
+#data = rel
 
 def getdata(symbol):
     url = f"""https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey=8SP5CFNLUHXR9F34&outputsize=compact"""
-    #data = requests.get(url).json()
-    print(data)
+    data = requests.get(url).json()
     return data['Time Series (Daily)']
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
+from operator import index
 from fulldf import full_df
 import pandas as pd
 from methods import find_trend,get_five_day_trend
 from stockname import names
-# symbols = ["RELIANCE.BSE",
-# "TTFl.BSE",
-# "JPPOWER.BSE",
-# "ASHSI.BSE",
-# "TATAPOWER.BSE",]
 
-symbols = ['test']
+Outdf = pd.DataFrame(columns=["Stock","Day1","Day2","Day3","Day4","Day5","Day6","Day7","weekly","15 day","Monthly"])
+symbols = [
+    "TTFl.BSE",
+    "JPPOWER.BSE",
+    "ASHSI.BSE",
+    "TATAPOWER.BSE"
+         ]
+
+# symbols = ['test']
 
 # symbols = names
 
 
 main_df = full_df(symbols)
-i = j = 0 
+i = j = k = 0 
 
-while i < main_df.shape[0]:
+for symbol in symbols:
     i = j + 30
     stock_df = main_df.iloc[j:i,:]
     j = i
@@ -24,8 +28,6 @@ while i < main_df.shape[0]:
     close_price = stock_df['Close'].to_list()
     
     five_data_trend = get_five_day_trend(close_price)
-
-    #print(five_data_trend)
 
     present_price = float(close_price[0])
 
@@ -37,3 +39,7 @@ while i < main_df.shape[0]:
 
     week_diff,month_diff,fort_diff = find_trend(present_price,last_week_price,last_15_price,last_month_price)
 
+    Outdf.loc[len(Outdf.index)] = [symbol,five_data_trend[0],five_data_trend[1],five_data_trend[2],five_data_trend[3],five_data_trend[4],five_data_trend[5],five_data_trend[6],week_diff,fort_diff,month_diff]
+
+
+print(Outdf.to_string())
