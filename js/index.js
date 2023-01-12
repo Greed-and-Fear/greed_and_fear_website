3bf02390e344adf8f4b19d5448f8d7cd10cb2c41
johnson845173 <rebeirojohnson@gmail.com>
2023-01-13 01:01:12 +0530

    added login page

--- js/index.js
+++ js/index.js
@@ -20,3 +20,14 @@ function clear_data()
     document.getElementById("name").value = ""
     document.getElementById("psw").value = ''
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#psw');
+
+  togglePassword.addEventListener('click', function() {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
