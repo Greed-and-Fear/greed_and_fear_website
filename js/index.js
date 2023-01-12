9db53b02dd7e7cb6841cdfc76082d284ca7f3e36
johnson845173 <rebeirojohnson@gmail.com>
2023-01-12 18:05:16 +0530

    added login page

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-    document.getElementById("body").style.backgroundColor = "red";
+function getVal() {
+    var name = document.getElementById("name").value;
+    var pass= document.getElementById("psw").value;
+    if(name === "admin"){
+        if(pass == "12345")
+        {
+            window.open("mainpage.html","_self");
+        } 
+    } 
+    else
+    {
+        window.alert("Incorrect Username or password");
+        clear_data()
+        
+    }
   }
 
-  function alertfun() {
-    window.alert(5 + 6)
-  }
+function clear_data()
+{
+    document.getElementById("name").value = ""
+    document.getElementById("psw").value = ''
+}
