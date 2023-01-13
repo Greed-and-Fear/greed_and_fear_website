ec0ac85c03ecae019064589ad491a1f83b83883d
johnson845173 <rebeirojohnson@gmail.com>
2023-01-13 23:20:05 +0530

    added sticky header

--- js/mainpage.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-    document.getElementById("body").style.backgroundColor = "red";
-  }
-
-  function alertfun() {
-    window.alert(5 + 6)
-  }
