c4e44c332f15e1161d115da54af21db75a8943c3
johnson845173 <rebeirojohnson@gmail.com>
2023-01-28 23:31:57 +0530

    added cross

--- js/apps.js
+++ js/apps.js
@@ -4,14 +4,14 @@ const observer = new IntersectionObserver((entries) => {
         if(entry.isIntersecting){
             entry.target.classList.add('show');
         }
-        else{
+        else{   
             entry.target.classList.remove('show');
         }
     });
   });
-
-const hiddenelement = document.querySelectorAll('.hidden');
-hiddenelement.forEach((el)=>observer.observe(el));
-
+  
 const hiddenelement2 = document.querySelectorAll('.hidden2');
+const hiddenelement = document.querySelectorAll('.hidden');
+
 hiddenelement2.forEach((el2)=>observer.observe(el2));
+hiddenelement.forEach((el)=>observer.observe(el));
