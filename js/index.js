8de5f804a8ecafa411561d6ef4c74242997464c2
johnson845173 <rebeirojohnson@gmail.com>
2023-01-29 23:05:44 +0530

    updated path

--- js/index.js
+++ js/index.js
@@ -46,7 +46,7 @@ async function get_intraday_stock() {
                     `
         <div class="card">
             <div class="stockname">${single_stock.stock_name}</div>
-            <div class="image"><img loading="lazy" alt =${single_stock.stock_name}-chart class="result-img" src="${single_stock.img_path}"></div>
+            <div class="image"><img alt =${single_stock.stock_name}-chart class="result-img" src="${single_stock.img_path}"></div>
             <div class="profit per">+${single_stock.percentage}%</div>
             <div class="profit amt">Profit : ${single_stock.profit}₹</div>
         </div>
