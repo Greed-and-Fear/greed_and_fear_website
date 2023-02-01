968b2d94413934d1613950848334f901df3b901c
johnson845173 <rebeirojohnson@gmail.com>
2023-02-01 20:37:24 +0530

    added black color

--- js/productpage.js
+++ js/productpage.js
@@ -1,4 +1,10 @@
 function load_product_page(product_id){
-    window.open('productpage.html','_self')
+    window.open("productpage.html","_self");
+    document.addEventListener("click", load_product)
+}
+
+
+function load_product(){
     console.log(product_id)
+    // document.getElementById('product-details').innerHTML = 'hello'
 }
