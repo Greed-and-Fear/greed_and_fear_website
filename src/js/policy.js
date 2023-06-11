a144a64af0332175e0638751eaa89687935d1141
johnson845173 <rebeirojohnson@gmail.com>
2023-06-11 14:16:05 +0530

    added tc

--- src/js/policy.js
+++ src/js/policy.js
@@ -36,12 +36,144 @@ function getCookie(cname) {
 function acceptCookieConsent(){
     deleteCookie('user_cookie_consent');
     setCookie('user_cookie_consent', 1, 30);
+
     document.getElementById("cookies-container").style.display = "none";
 }
 
 function rejectCookieConsent(){
     document.getElementById("cookies-container").style.display = "none";
 }
+
+
+
+cookie_msg = `<div class="cookie-doc-container-header">COOKIE POLICY</div>
+<div class="cookie-doc-container-sub-head date-of-policy">Last updated June 10, 2023</div>
+
+<div class="cookie-text">This Cookie Policy explains how Termly ("Company," "we," "us," and "our") uses cookies
+    and similar technologies to recognize you when you visit our website at https://greedandfear.fun
+    ("Website"). It explains what these technologies are and why we use them, as well as your rights to control
+    our use of them.</div>
+
+<div class="cookie-text">In some cases we may use cookies to collect personal information, or that becomes
+    personal information if we combine it with other information.</div>
+
+<div class="cookie-doc-container-sub-head">What are cookies?</div>
+
+<div class="cookie-text">Cookies are small data files that are placed on your computer or mobile device when you
+    visit a website. Cookies are widely used by website owners in order to make their websites work, or to work
+    more efficiently, as well as to provide reporting information.</div>
+
+<div class="cookie-text">Cookies set by the website owner (in this case, Termly) are called "first-party
+    cookies." Cookies set by parties other than the website owner are called "third-party cookies." Third-party
+    cookies enable third-party features or functionality to be provided on or through the website (e.g.,
+    advertising, interactive content, and analytics). The parties that set these third-party cookies can
+    recognize your computer both when it visits the website in question and also when it visits certain other
+    websites.</div>
+
+<div class="cookie-doc-container-sub-head">Why do we use cookies?</div>
+
+<div class="cookie-text">We use first- and third-party cookies for several reasons. Some cookies are required
+    for technical reasons in order for our Website to operate, and we refer to these as "essential" or "strictly
+    necessary" cookies. Other cookies also enable us to track and target the interests of our users to enhance
+    the experience on our Online Properties. Third parties serve cookies through our Website for advertising,
+    analytics, and other purposes. This is described in more detail below.</div>
+
+<div class="cookie-doc-container-sub-head">How can I control cookies?</div>
+
+<div class="cookie-text">You have the right to decide whether to accept or reject cookies. You can exercise your
+    cookie rights by setting your preferences in the Cookie Consent Manager. The Cookie Consent Manager allows
+    you to select which categories of cookies you accept or reject. Essential cookies cannot be rejected as they
+    are strictly necessary to provide you with services.</div>
+
+<div class="cookie-text">The Cookie Consent Manager can be found in the notification banner and on our website.
+    If you choose to reject cookies, you may still use our website though your access to some functionality and
+    areas of our website may be restricted. You may also set or amend your web browser controls to accept or
+    refuse cookies.</div>
+
+<div class="cookie-text">The specific types of first- and third-party cookies served through our Website and the
+    purposes they perform are described in the table below (please note that the specific cookies served may
+    vary depending on the specific Online Properties you visit):</div>
+
+<div class="cookie-doc-container-sub-head">Essential website cookies:</div>
+
+<div class="cookie-text">These cookies are strictly necessary to provide you with services available through our
+    Website and to use some of its features, such as access to secure areas.</div>
+
+<div class="cookie-box">
+    Name:user_cookie_consent<br>
+    Purpose:This cookie holds wether user accepted the cookies.<br>
+    Provider:.greeandfear.fun<br>
+    Country:India<br>
+    Type:https_cookie<br>
+    Expires in:30 days<br>
+</div>
+
+<div class="cookie-close"><button class="cookie-close-btn" onclick="hidecookiepolicy()">Close</button></div>`
+
+tc_msg = `
+<div class="cookie-doc-container-header">TERMS & CONDITIONS</div>
+<div class="cookie-doc-container-sub-head date-of-policy">Last updated June 10, 2023</div>
+
+<div class="cookie-text">This Cookie Policy explains how Termly ("Company," "we," "us," and "our") uses cookies
+    and similar technologies to recognize you when you visit our website at https://greedandfear.fun
+    ("Website"). It explains what these technologies are and why we use them, as well as your rights to control
+    our use of them.</div>
+
+<div class="cookie-text">In some cases we may use cookies to collect personal information, or that becomes
+    personal information if we combine it with other information.</div>
+
+<div class="cookie-doc-container-sub-head">What are cookies?</div>
+
+<div class="cookie-text">Cookies are small data files that are placed on your computer or mobile device when you
+    visit a website. Cookies are widely used by website owners in order to make their websites work, or to work
+    more efficiently, as well as to provide reporting information.</div>
+
+<div class="cookie-text">Cookies set by the website owner (in this case, Termly) are called "first-party
+    cookies." Cookies set by parties other than the website owner are called "third-party cookies." Third-party
+    cookies enable third-party features or functionality to be provided on or through the website (e.g.,
+    advertising, interactive content, and analytics). The parties that set these third-party cookies can
+    recognize your computer both when it visits the website in question and also when it visits certain other
+    websites.</div>
+
+<div class="cookie-doc-container-sub-head">Why do we use cookies?</div>
+
+<div class="cookie-text">We use first- and third-party cookies for several reasons. Some cookies are required
+    for technical reasons in order for our Website to operate, and we refer to these as "essential" or "strictly
+    necessary" cookies. Other cookies also enable us to track and target the interests of our users to enhance
+    the experience on our Online Properties. Third parties serve cookies through our Website for advertising,
+    analytics, and other purposes. This is described in more detail below.</div>
+
+<div class="cookie-doc-container-sub-head">How can I control cookies?</div>
+
+<div class="cookie-text">You have the right to decide whether to accept or reject cookies. You can exercise your
+    cookie rights by setting your preferences in the Cookie Consent Manager. The Cookie Consent Manager allows
+    you to select which categories of cookies you accept or reject. Essential cookies cannot be rejected as they
+    are strictly necessary to provide you with services.</div>
+
+<div class="cookie-text">The Cookie Consent Manager can be found in the notification banner and on our website.
+    If you choose to reject cookies, you may still use our website though your access to some functionality and
+    areas of our website may be restricted. You may also set or amend your web browser controls to accept or
+    refuse cookies.</div>
+
+<div class="cookie-text">The specific types of first- and third-party cookies served through our Website and the
+    purposes they perform are described in the table below (please note that the specific cookies served may
+    vary depending on the specific Online Properties you visit):</div>
+
+<div class="cookie-doc-container-sub-head">Essential website cookies:</div>
+
+<div class="cookie-text">These cookies are strictly necessary to provide you with services available through our
+    Website and to use some of its features, such as access to secure areas.</div>
+
+<div class="cookie-box">
+    Name:user_cookie_consent<br>
+    Purpose:This cookie holds wether user accepted the cookies.<br>
+    Provider:.greeandfear.fun<br>
+    Country:India<br>
+    Type:https_cookie<br>
+    Expires in:30 days<br>
+</div>
+
+<div class="cookie-close"><button class="cookie-close-btn" onclick="hidetcpolicy()">Close</button></div>`
 
 let cookie_consent = getCookie("user_cookie_consent");
 if(cookie_consent != ""){
@@ -55,5 +187,16 @@ function hidecookiepolicy(){
 }
 
 function showcookiepolicy(){
+    document.getElementById("cookie-doc-container").innerHTML = cookie_msg;     
     document.getElementById("cookie-doc-container").style.display = "block";     
 }
+
+function hidetcpolicy(){
+    document.getElementById("cookie-doc-container-tc").style.display = "none";     
+}
+
+function showtcpolicy(){
+    document.getElementById("cookie-doc-container-tc").innerHTML = tc_msg;     
+    document.getElementById("cookie-doc-container-tc").style.display = "block";     
+}
+
