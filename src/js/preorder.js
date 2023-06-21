6314b0fd01109c5f31a07807161aff742ba65b71
johnson845173 <rebeirojohnson@gmail.com>
2023-06-21 23:22:50 +0530

    added ui for preorder

--- src/js/preorder.js
+++ src/js/preorder.js
@@ -1,21 +1,12 @@
-function change_price()
+function send_response()
 {
-    const switch_button = document.getElementById('switcher');
+    // const switch_button = document.getElementById('switcher');
 
-    const basic = document.getElementById('basic');
-    const prime = document.getElementById('premium');
-    const elite = document.getElementById('elite');
-    
-    if (switch_button.checked == false){
-        basic.innerHTML = "₹299/week";
-        prime.innerHTML = "₹349/week";
-        elite.innerHTML = "₹399/week";
-        
-    } else 
-    {
-          basic.innerHTML = "₹799/Monthly";
-          prime.innerHTML = "₹999/Montly";
-          elite.innerHTML = "₹1099/Montly";
-      }
+    const basic = document.getElementById('basic').checked;
+    const prime = document.getElementById('premium').checked;
+    const elite = document.getElementById('elite').checked;
+
+    console.log(basic)
+  
 
 }
