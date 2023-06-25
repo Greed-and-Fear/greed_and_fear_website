d7d770e1b367163242a81722af114122159b4e01
johnson845173 <rebeirojohnson@gmail.com>
2023-06-25 15:17:27 +0530

    added preorder pag

--- src/js/preorder.js
+++ src/js/preorder.js
@@ -1,4 +1,4 @@
-function send_response()
+function send_response2()
 {
     // const switch_button = document.getElementById('switcher');
 
@@ -6,7 +6,79 @@ function send_response()
     const prime = document.getElementById('premium').checked;
     const elite = document.getElementById('elite').checked;
 
-    console.log(basic)
-  
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.greedandfear.fun/api/preorder/",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache",
+            "Sec-Fetch-Mode":"no-cors",
+            'X-Requested-With':'XMLHttpRequest',
+            'Access-Control-Allow-Origin':'*'
+
+        },
+        "data": JSON.stringify({ 
+            "user_name":"johnson",
+            "phone":12345678,
+            "email":"dummy@gma",
+            "basic":"true",
+            "premium":"false",
+            "elite":"true",
+            "ew":"true",
+            "fib":"true",
+        }
+        )
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+
+}
+
+function send_response()
+{
+
+    const basic = document.getElementById('basic').checked;
+    const prime = document.getElementById('premium').checked;
+    const elite = document.getElementById('elite').checked;
+    const ew = document.getElementById('ew').checked;
+    const fib = document.getElementById('fib').checked;
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+fetch('https://api.greedandfear.fun/api/preorder/', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Sec-Fetch-Site':'cross-site'
+    },
+    body: JSON.stringify(
+        { 
+            "user_name":name,
+            "phone":phone,
+            "email":email,
+            "basic":basic,
+            "premium":prime,
+            "elite":elite,
+            "ew":ew,
+            "fib":fib,
+        }
+        )
+})
+   .then(response => response.json())
+   .then(response => console.log(JSON.stringify(response)))
+
+   out = `
+   <div class="thank-you">Thank you for Registering.
+   <br> Our Team will reach out to you very soon.
+   </div>
+   `
+
+   document.getElementById('reg-container').innerHTML = out;
 
 }
--- tele.py
+++ tele.py
@@ -2,18 +2,21 @@ import requests
 
 url = "https://api.telegram.org/bot6236663076:AAGct2VT2I3j9rsY9ja-KANqJbbSLhEHWB0/sendPhoto"
 
+count_url = "https://api.telegram.org/bot6236663076:AAGct2VT2I3j9rsY9ja-KANqJbbSLhEHWB0/getChatMembersCount"
+
 data_to_send = {
     "chat_id": "@weather845173",
     # "photo":"https://www.dropbox.com/s/al5j70vqp0u577f/20221001_210356.jpg?dl=0"
-    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1122px-Wikipedia-logo-v2.png"
+    # "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1122px-Wikipedia-logo-v2.png"
         }
 
 headers = {
-    "Content-type": "image/png",
+    "Content-type": "application/json",
     "Content-Length": "97"
 }
 
 
-data = requests.post(url=url,json=data_to_send,headers=headers)
+data = requests.post(url=count_url,json=data_to_send,headers=headers)
 
 print(data.text)
+
