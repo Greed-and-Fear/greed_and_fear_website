a45b2203ac8834d17b21a2350e942a763f6cf0db
johnson845173 <rebeirojohnson@gmail.com>
2023-07-09 15:41:58 +0530

    added book page

--- src/js/productpage.js
+++ src/js/productpage.js
@@ -19,3 +19,25 @@ function change_price()
       }
 
 }
+
+function change_price_book()
+{
+    const switch_button = document.getElementById('switcher');
+
+    const basic = document.getElementById('basic');
+    const prime = document.getElementById('premium');
+    const elite = document.getElementById('elite');
+    
+    if (switch_button.checked == false){
+        basic.innerHTML = "₹299/week";
+        prime.innerHTML = "₹399/week";
+        elite.innerHTML = "₹599/week";
+        
+    } else 
+    {
+          basic.innerHTML = "₹897/Monthly";
+          prime.innerHTML = "₹1297/Montly";
+          elite.innerHTML = "₹1797/Montly";
+      }
+
+}
