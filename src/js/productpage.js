3a3f2171db996004ed1973c6e16d3e8c3ca7843d
johnson845173 <rebeirojohnson@gmail.com>
2023-08-12 23:10:41 +0530

    added page to product page

--- src/js/productpage.js
+++ src/js/productpage.js
@@ -5,17 +5,26 @@ function change_price()
     const basic = document.getElementById('basic');
     const prime = document.getElementById('premium');
     const elite = document.getElementById('elite');
+    const baisclink = document.getElementById('baisclink');
+    const premiumlink = document.getElementById('premiumlink');
+    const elitelink = document.getElementById('elitelink');
     
     if (switch_button.checked == false){
         basic.innerHTML = "₹299/week";
         prime.innerHTML = "₹399/week";
         elite.innerHTML = "₹599/week";
+        baisclink.innerHTML = '<a href="orderpages/basicweek.html"><button class="features-btn">Buy Now</button></a>';
+        premiumlink.innerHTML = '<a href="orderpages/premiumweek.html"><button class="features-btn">Buy Now</button></a>';
+        elitelink.innerHTML = '<a href="orderpages/eliteweek.html"><button class="features-btn">Buy Now</button></a>';
         
     } else 
     {
           basic.innerHTML = "₹897/Monthly";
           prime.innerHTML = "₹1197/Montly";
           elite.innerHTML = "₹1797/Montly";
+          baisclink.innerHTML = '<a href="orderpages/basicmonth.html"><button class="features-btn">Buy Now</button></a>';
+          premiumlink.innerHTML = '<a href="orderpages/premiummonth.html"><button class="features-btn">Buy Now</button></a>';
+          elitelink.innerHTML = '<a href="orderpages/elitewonthlu.html"><button class="features-btn">Buy Now</button></a>';
       }
 
 }
