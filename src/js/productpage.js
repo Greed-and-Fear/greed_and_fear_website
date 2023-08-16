b1a9e4befb23eda04a72e5acf3773bc2b6a548a0
johnson845173 <rebeirojohnson@gmail.com>
2023-08-16 23:15:16 +0530

    changed price of link

--- src/js/productpage.js
+++ src/js/productpage.js
@@ -10,18 +10,18 @@ function change_price()
     const elitelink = document.getElementById('elitelink');
     
     if (switch_button.checked == false){
-        basic.innerHTML = "₹299/week";
-        prime.innerHTML = "₹399/week";
-        elite.innerHTML = "₹599/week";
+        basic.innerHTML = "₹399/week";
+        prime.innerHTML = "₹599/week";
+        elite.innerHTML = "₹799/week";
         baisclink.innerHTML = '<a href="orderpages/basicweek.html"><button class="features-btn">Buy Now</button></a>';
         premiumlink.innerHTML = '<a href="orderpages/premiumweek.html"><button class="features-btn">Buy Now</button></a>';
         elitelink.innerHTML = '<a href="orderpages/eliteweek.html"><button class="features-btn">Buy Now</button></a>';
         
     } else 
     {
-        basic.innerHTML = "₹897/Monthly";
-        prime.innerHTML = "₹1197/Montly";
-        elite.innerHTML = "₹1797/Montly";
+        basic.innerHTML = "₹1399/Monthly";
+        prime.innerHTML = "₹2099/Montly";
+        elite.innerHTML = "₹2899/Montly";
         baisclink.innerHTML = '<a href="orderpages/basicweek.html"><button class="features-btn">Buy Now</button></a>';
         premiumlink.innerHTML = '<a href="orderpages/premiumweek.html"><button class="features-btn">Buy Now</button></a>';
         elitelink.innerHTML = '<a href="orderpages/eliteweek.html"><button class="features-btn">Buy Now</button></a>';
