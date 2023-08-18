da47094f3e20cb2f24da895992ee21177a99a680
johnson845173 <rebeirojohnson@gmail.com>
2023-08-19 00:46:38 +0530

    added launch page

--- src/js/launch.js
+++ src/js/launch.js
@@ -1,6 +1,6 @@
 function get_time_left(){
 // Set the date we're counting down to
-var countDownDate = new Date("July 1, 2023 16:00:00").getTime();
+var countDownDate = new Date("Sep 1, 2023 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
